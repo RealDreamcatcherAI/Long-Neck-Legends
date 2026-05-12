9ba5470ed47191a0f40f683a27f845182f8bbfb9
Glitch <aglitcheddream@gmail.com>
2026-05-12 00:30:06 -0500

    Update start.js

--- api/auth/discord/start.js
+++ api/auth/discord/start.js
@@ -12,7 +12,7 @@ export default async function handler(req, res) {
     redirect_uri:  process.env.DISCORD_REDIRECT_URI,
     response_type: "code",
     scope:         "identify",
-    state
+    state,
     prompt:        "none"
   });
 
